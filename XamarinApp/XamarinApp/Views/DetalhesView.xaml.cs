d313ee31335be737e0ce8f8b966287ae35351024
Jonas Augusto <95719426+jonasaugust1@users.noreply.github.com>
2023-07-06 20:08:45 -0300

    Implementing TwoWay Model Binding and OnPropertyChanged

--- XamarinApp/XamarinApp/Views/DetalhesView.xaml.cs
+++ XamarinApp/XamarinApp/Views/DetalhesView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Xamarin.Forms;
+﻿using System.Globalization;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using XamarinApp.Models;
 
@@ -7,13 +8,100 @@ namespace XamarinApp.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class DetalhesView : ContentPage
     {
+        private const decimal FREIO_ABS = 800.00M;
+        private const decimal AR_CONDICIONADO = 1000.00M;
+        private const decimal DISPOSITIVO_MULTIMIDIA = 550.00M;
         public Veiculo Veiculo { get; set; }
+        public string TextoFreioABS
+        {
+            get
+            {
+                return $"Freio ABS R$ {FREIO_ABS}";
+            }
+        }
+        public string TextoArCondicionado
+        {
+            get
+            {
+                return $"Ar-condicionado R$ {AR_CONDICIONADO}";
+            }
+        }
+        public string TextoDispositivoMultimidia
+        {
+            get
+            {
+                return $"Dispositivo multimídia R$ {DISPOSITIVO_MULTIMIDIA}";
+            }
+        }
+
+        bool _TemFreioABS;
+        public bool TemFreioABS 
+        { 
+            get
+            {
+                return _TemFreioABS;
+            }
+            set
+            {
+                _TemFreioABS = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(ValorTotal));
+            }
+        }
+
+        bool _TemArCondicionado;
+        public bool TemArCondicionado 
+        { 
+            get
+            {
+                return _TemArCondicionado;
+            }
+            set
+            {
+                _TemArCondicionado = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(ValorTotal));
+            }
+        }
+
+        bool _TemDispositivoMultimidia;
+        public bool TemDispositivoMultimidia
+        { 
+            get
+            {
+                return _TemDispositivoMultimidia;
+            }
+            set
+            {
+                _TemDispositivoMultimidia = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(ValorTotal));
+            } 
+        }
+        public string ValorTotal
+        {
+            get
+            {
+                decimal valorTotal = Veiculo.Preco;
+
+                valorTotal += TemFreioABS ? FREIO_ABS : 0;
+
+                valorTotal += TemArCondicionado ? AR_CONDICIONADO : 0;
+
+                valorTotal += TemDispositivoMultimidia ? DISPOSITIVO_MULTIMIDIA : 0;
+
+                return $"Total: {valorTotal.ToString("C2", CultureInfo.CurrentCulture)}";
+            }
+        }
+
         public DetalhesView(Veiculo veiculo)
         {
             InitializeComponent();
             Title = veiculo.Nome;
 
             Veiculo = veiculo;
+
+            BindingContext = this;
         }
 
         private void BtnProximo_Clicked(object sender, System.EventArgs e)
