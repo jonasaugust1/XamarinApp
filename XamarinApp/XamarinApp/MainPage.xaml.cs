e21e6b556a5d6a09abc53c5697160a8558c2f35a
Jonas Augusto <95719426+jonasaugust1@users.noreply.github.com>
2023-07-06 18:17:01 -0300

    Add project files.

---
+++ XamarinApp/XamarinApp/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+using XamarinApp.Models;
+
+namespace XamarinApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public List<Veiculo> Veiculos { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Veiculos = new List<Veiculo>
+            {
+                new Veiculo
+                {
+                    Nome = "Azera V6",
+                    Preco = 60000
+                },
+                new Veiculo
+                {
+                    Nome = "Fiesta 2.0",
+                    Preco = 50000
+                },
+                new Veiculo
+                {
+                    Nome = "HB20 S",
+                    Preco = 32000
+                }
+            };
+
+            BindingContext = this;
+        }
+    }
+}
---
+++ XamarinApp/XamarinApp/Models/Veiculo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinApp.Models
+{
+    public class Veiculo
+    {
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
