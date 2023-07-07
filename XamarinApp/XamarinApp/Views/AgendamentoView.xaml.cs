ead37a1bdba00e2a600743bd792f2c944600cf16
Jonas Augusto <95719426+jonasaugust1@users.noreply.github.com>
2023-07-07 08:55:46 -0300

    Implementing Date and Time Pickers

--- XamarinApp/XamarinApp/Views/AgendamentoView.xaml.cs
+++ XamarinApp/XamarinApp/Views/AgendamentoView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using XamarinApp.Models;
 
@@ -7,10 +8,65 @@ namespace XamarinApp.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AgendamentoView : ContentPage
     {
+        public Veiculo Veiculo { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+
+        DateTime _Data;
+        public DateTime Data
+        {
+            get
+            {
+                return _Data;
+            }
+            set
+            {
+                _Data = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public DateTime DataMinima
+        {
+            get
+            {
+                return DateTime.Now;
+            }
+        }
+
+        TimeSpan _Hora;
+        public TimeSpan Hora
+        {
+            get
+            {
+                return _Hora;
+            }
+            set
+            {
+                _Hora = value;
+                OnPropertyChanged();
+            }
+        }
         public AgendamentoView(Veiculo veiculo)
         {
             InitializeComponent();
             Title = veiculo.Nome;
+            Veiculo = veiculo;
+
+            BindingContext = this;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Agendamento", 
+$@"
+Nome: {Nome}
+Telefone: {Telefone}
+Email: {Email}
+Data: {Data.ToString("dd/MM/yyyy")}
+Hora: {Hora}
+Modelo: {Veiculo.Nome}", "Ok");
         }
     }
 }
