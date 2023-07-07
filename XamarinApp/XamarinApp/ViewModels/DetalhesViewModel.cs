cdc6c619b9ca0b043032d6d5b6bee04d175db22e
Jonas Augusto <95719426+jonasaugust1@users.noreply.github.com>
2023-07-07 20:44:54 -0300

    Refactoring DetalhesView logic to DetalhesViewModel and implementing INotifyPropertyChanged in The ViewModel

---
+++ XamarinApp/XamarinApp/ViewModels/DetalhesViewModel.cs
@@ -0,0 +1,92 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using XamarinApp.Models;
+
+namespace XamarinApp.ViewModels
+{
+    public class DetalhesViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public Veiculo Veiculo { get; set; }
+        public string TextoFreioABS
+        {
+            get
+            {
+                return $"Freio ABS R$ {Veiculo.FREIO_ABS}";
+            }
+        }
+        public string TextoArCondicionado
+        {
+            get
+            {
+                return $"Ar-condicionado R$ {Veiculo.AR_CONDICIONADO}";
+            }
+        }
+        public string TextoDispositivoMultimidia
+        {
+            get
+            {
+                return $"Dispositivo multimídia R$ {Veiculo.DISPOSITIVO_MULTIMIDIA}";
+            }
+        }
+        public bool TemFreioABS
+        {
+            get
+            {
+                return Veiculo.TemFreioABS;
+            }
+            set
+            {
+                Veiculo.TemFreioABS = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(ValorTotal));
+            }
+        }
+        public bool TemArCondicionado
+        {
+            get
+            {
+                return Veiculo.TemArCondicionado;
+            }
+            set
+            {
+                Veiculo.TemArCondicionado = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(ValorTotal));
+            }
+        }
+        public bool TemDispositivoMultimidia
+        {
+            get
+            {
+                return Veiculo.TemDispositivoMultimidia;
+            }
+            set
+            {
+                Veiculo.TemDispositivoMultimidia = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(ValorTotal));
+            }
+        }
+        public string ValorTotal
+        {
+            get
+            {
+                return Veiculo.ValorTotal;
+            }
+        }
+        public DetalhesViewModel(Veiculo veiculo)
+        {
+            Veiculo = veiculo;
+        }
+
+        /*
+        Se nada for passado no parâmetro o CallerMemberName vai assumir 
+        que o name é o da própria propriedade que está chamando o método
+        */
+        public void OnPropertyChanged([CallerMemberName]string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
--- XamarinApp/XamarinApp/Views/DetalhesView.xaml.cs
+++ XamarinApp/XamarinApp/Views/DetalhesView.xaml.cs
@@ -1,7 +1,7 @@
-﻿using System.Globalization;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using XamarinApp.Models;
+using XamarinApp.ViewModels;
 
 namespace XamarinApp.Views
 {
@@ -9,82 +9,12 @@ namespace XamarinApp.Views
     public partial class DetalhesView : ContentPage
     {
         public Veiculo Veiculo { get; set; }
-        public string TextoFreioABS
-        {
-            get
-            {
-                return $"Freio ABS R$ {Veiculo.FREIO_ABS}";
-            }
-        }
-        public string TextoArCondicionado
-        {
-            get
-            {
-                return $"Ar-condicionado R$ {Veiculo.AR_CONDICIONADO}";
-            }
-        }
-        public string TextoDispositivoMultimidia
-        {
-            get
-            {
-                return $"Dispositivo multimídia R$ {Veiculo.DISPOSITIVO_MULTIMIDIA}";
-            }
-        }
-        public bool TemFreioABS 
-        { 
-            get
-            {
-                return Veiculo.TemFreioABS;
-            }
-            set
-            {
-                Veiculo.TemFreioABS = value;
-                OnPropertyChanged();
-                OnPropertyChanged(nameof(ValorTotal));
-            }
-        }
-        public bool TemArCondicionado 
-        { 
-            get
-            {
-                return Veiculo.TemArCondicionado;
-            }
-            set
-            {
-                Veiculo.TemArCondicionado = value;
-                OnPropertyChanged();
-                OnPropertyChanged(nameof(ValorTotal));
-            }
-        }
-        public bool TemDispositivoMultimidia
-        { 
-            get
-            {
-                return Veiculo.TemDispositivoMultimidia;
-            }
-            set
-            {
-                Veiculo.TemDispositivoMultimidia = value;
-                OnPropertyChanged();
-                OnPropertyChanged(nameof(ValorTotal));
-            } 
-        }
-        public string ValorTotal
-        {
-            get
-            {
-                return Veiculo.ValorTotal;
-            }
-        }
-
         public DetalhesView(Veiculo veiculo)
         {
             InitializeComponent();
             Title = veiculo.Nome;
-
             Veiculo = veiculo;
-
-            BindingContext = this;
+            BindingContext = new DetalhesViewModel(veiculo);
         }
 
         private void BtnProximo_Clicked(object sender, System.EventArgs e)
