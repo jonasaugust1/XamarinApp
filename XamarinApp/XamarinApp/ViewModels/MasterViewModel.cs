fdf2bdb3b2b98a3a46f459a1490c53b1b1369011
Jonas Augusto <95719426+jonasaugust1@users.noreply.github.com>
2023-08-05 15:50:02 -0300

    Editing Perfil

--- XamarinApp/XamarinApp/ViewModels/MasterViewModel.cs
+++ XamarinApp/XamarinApp/ViewModels/MasterViewModel.cs
@@ -1,13 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Globalization;
 using System.Windows.Input;
 using Xamarin.Forms;
 using XamarinApp.Models;
 
 namespace XamarinApp.ViewModels
 {
-    public class MasterViewModel
+    public class MasterViewModel : BaseViewModel
     {
         public string Nome
         {
@@ -15,10 +14,10 @@ namespace XamarinApp.ViewModels
             set { _Usuario.Nome = value; }
         }
 
-        public DateTime DataNascimento
+        public string DataNascimento
         {
-            get { return _Usuario.DataNascimento; }
-            set { _Usuario.DataNascimento = value; }
+            get { return _Usuario.DataNascimento.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture); }
+            set { _Usuario.DataNascimento = DateTime.Parse(value); }
         }
 
         public string Telefone
@@ -33,9 +32,43 @@ namespace XamarinApp.ViewModels
             set { _Usuario.Email = value; }
         }
 
+        private bool _InputAtivado = false;
+        public bool InputAtivado
+        {
+            get { return _InputAtivado; }
+            private set 
+            {
+                _InputAtivado = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _BtnEditarVisivel = true;
+        public bool BtnEditarVisivel
+        {
+            get { return _BtnEditarVisivel; }
+            private set
+            {
+                _BtnEditarVisivel = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _BtnSalvarVisivel = false;
+        public bool BtnSalvarVisivel
+        {
+            get { return _BtnSalvarVisivel; }
+            private set
+            {
+                _BtnSalvarVisivel = value;
+                OnPropertyChanged();
+            }
+        }
+
         private readonly Usuario _Usuario;
         public ICommand EditarPerfilCommand {get; private set;}
-        public ICommand SalvarPerfilCommand { get; private set; }
+        public ICommand SalvarCommand { get; private set; }
+        public ICommand EditarCommand { get; private set; }
 
         public MasterViewModel(Usuario usuario)
         {
@@ -49,9 +82,20 @@ namespace XamarinApp.ViewModels
                 MessagingCenter.Send(usuario, "EditarPerfil");
             });
 
-            SalvarPerfilCommand = new Command(() =>
+            SalvarCommand = new Command(() =>
             {
                 MessagingCenter.Send(usuario, "SalvarPerfil");
+
+                InputAtivado = false;
+                BtnSalvarVisivel = false;
+                BtnEditarVisivel = true;
+            });
+
+            EditarCommand = new Command(() =>
+            {
+                InputAtivado = true;
+                BtnSalvarVisivel = true;
+                BtnEditarVisivel = false;
             });
         }
     }
